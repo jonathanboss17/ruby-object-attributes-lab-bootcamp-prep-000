ceb70abe9fe67976d9f7e8f61b1c0da0d8bc7d6a
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-15 09:26:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,20 @@ class Person
   end
   
   def name 
-    
+    @name 
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def job 
+    @job 
+  end
+  
+  def job=(new_job)
+    @job = job
+  end
   
   
 end
